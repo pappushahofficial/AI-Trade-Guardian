c9921e160603c4151054803ae2d993f25fb53dbc
Pappu Shah <147172502+pappushahofficial@users.noreply.github.com>
2026-06-05 22:52:35 +0545

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -291,10 +291,25 @@ def get_data(coin_symbol=None):
     ]
 
 
-    df["close"] = (
-        df["close"]
-        .astype(float)
-    )
+    df["open"] = (
+    df["open"]
+    .astype(float)
+)
+
+df["high"] = (
+    df["high"]
+    .astype(float)
+)
+
+df["low"] = (
+    df["low"]
+    .astype(float)
+)
+
+df["close"] = (
+    df["close"]
+    .astype(float)
+)
 
 
     return df
