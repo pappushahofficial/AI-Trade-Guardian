78e0c81b30134198565a54ec5a44338a3ec660b4
Pappu Shah <147172502+pappushahofficial@users.noreply.github.com>
2026-06-19 19:02:38 +0545

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -281,6 +281,16 @@ elif menu == "📊 Trade Logs":
     if len(logs) == 0:
         st.info("No trades executed yet")
     else:
+        logs.columns = [
+            "Time",
+            "Asset",
+            "Decision",
+            "Entry",
+            "Stop Loss",
+            "Take Profit",
+            "Confidence"
+        ]
+
         st.dataframe(
             logs,
             use_container_width=True
